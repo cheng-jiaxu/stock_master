41dfc2ee804e620c2926f7820381f4549b0b4a82
aijktm <aijktm@outlook.com0;git config --global user.name aijktmgit config --global user.email aijktm@outlook.com>
2026-08-03 16:28:10 +0800

    feat: 在utils引入雪花算法工具类

---
+++ stock_common/src/main/java/com/ming/stock/constant/StockConstant.java
@@ -0,0 +1,17 @@
+package com.ming.stock.constant;
+
+public class StockConstant {
+    /**
+     * 定义校验码的前缀
+     */
+    public static final String CHECK_PREFIX="CK: ";
+    /**
+     * http请求头携带token信息 key
+     */
+    public static final String TOKEN_HEADER = "authorization";
+    /**
+     * 缓存股票相关信息命名前缀
+     */
+    public static final String STOCK = "stock";
+
+}
