0f1c399977e0aaa9d686d5b5a141e0b193876df3
cheng-jiaxu <aijktm@outlook.com>
2026-08-06 16:49:59 +0800

    feat(stock): 定义涨幅榜以及涨幅榜分页服务访问接口方法

--- stock_backend/src/main/java/com/ming/stock/controller/StockController.java
+++ stock_backend/src/main/java/com/ming/stock/controller/StockController.java
@@ -2,11 +2,14 @@ package com.ming.stock.controller;
 
 import com.ming.stock.domain.InnerMarketDomain;
 import com.ming.stock.domain.StockBlockDomain;
+import com.ming.stock.domain.StockUpdownDomain;
 import com.ming.stock.service.StockService;
+import com.ming.stock.vo.resp.PageResult;
 import com.ming.stock.vo.resp.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -29,4 +32,14 @@ public class StockController {
     public R<List<StockBlockDomain>> sectorAllLimit(){
         return  stockService.sectorAllLimit();
     }
+
+    @GetMapping("/stock/all")
+    public R<PageResult<StockUpdownDomain>> getStockInfoByPage(@RequestParam(name = "Page",required = false,defaultValue = "1") Integer Page, @RequestParam(name = "PageSize",required = false,defaultValue = "20") Integer PageSize){
+        return stockService.getStockInfoByPage(Page,PageSize);
+    }
+    @GetMapping("stock/increase")
+    public R<List<StockUpdownDomain>> getTopStocksByIncrease(){
+        return stockService.getTopStocksByIncrease();
+    }
+
 }
