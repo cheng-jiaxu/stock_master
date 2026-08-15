9e15ab7863d1ba247bb7d971d4272ed2d1c5b719
cheng-jiaxu <aijktm@outlook.com>
2026-08-16 04:17:44 +0800

    feat(job): 添加parserstockinfoUtil bean

--- stock_job/src/main/java/com/ming/stock/config/CommonConfig.java
+++ stock_job/src/main/java/com/ming/stock/config/CommonConfig.java
@@ -1,6 +1,7 @@
 package com.ming.stock.config;
 
 import com.ming.stock.utils.IdWorker;
+import com.ming.stock.utils.ParserStockInfoUtil;
 import com.ming.stock.vo.StockInfoConfig;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -12,5 +13,9 @@ public class CommonConfig {
     @Bean
     public IdWorker idWorker(){
         return new IdWorker(1L,2L);
+    }
+    @Bean
+    public ParserStockInfoUtil parserStockInfoUtil(){
+        return new ParserStockInfoUtil(idWorker());
     }
 }
