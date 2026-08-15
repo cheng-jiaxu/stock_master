87bcc4e6ed2c0314efc9b5c4efc8b2b48a7cf0a8
cheng-jiaxu <aijktm@outlook.com>
2026-08-16 01:45:49 +0800

    feat(config): 配置id生成器和resttemplateBean

---
+++ stock_job/src/main/java/com/ming/stock/config/CommonConfig.java
@@ -0,0 +1,16 @@
+package com.ming.stock.config;
+
+import com.ming.stock.utils.IdWorker;
+import com.ming.stock.vo.StockInfoConfig;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@EnableConfigurationProperties(StockInfoConfig.class)
+@Configuration
+public class CommonConfig {
+
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker(1L,2L);
+    }
+}
