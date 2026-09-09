0b602ee81866083af8ef6b89fc51e93c4c433fd3
cheng-jiaxu <aijktm@outlook.com>
2026-09-09 21:20:08 +0800

    feat(job): 新增线程池配置属性类

---
+++ stock_common/src/main/java/com/ming/stock/vo/TaskThreadPoolInfo.java
@@ -0,0 +1,14 @@
+package com.ming.stock.vo;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "task.pool")
+@Data
+public class TaskThreadPoolInfo {
+
+    private  Integer corePoolSize;
+    private  Integer maxPoolSize;
+    private  Integer keepAliveSeconds;
+    private  Integer queueCapacity;
+}
